6ea25afb658d747710d644a2777e9d9df0602195
cecobask <baskski@gmail.com>
2019-09-04 20:44:13 +0100

    Users can now save words to their own collection. Progress bar shows while data is being loaded.

--- app/src/main/java/bask/learnbulgarian/fragments/WordOfTheDayFragment.kt
+++ app/src/main/java/bask/learnbulgarian/fragments/WordOfTheDayFragment.kt
@@ -1,5 +1,7 @@
 package bask.learnbulgarian.fragments
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.net.Uri
@@ -8,15 +10,15 @@ import android.os.StrictMode
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import bask.learnbulgarian.R
 import bask.learnbulgarian.models.WordOfTheDay
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import timber.log.Timber
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
@@ -24,8 +26,9 @@ import java.time.format.FormatStyle
 
 class WordOfTheDayFragment: Fragment() {
 
-    private lateinit var database: FirebaseDatabase
+    private lateinit var databaseRef: DatabaseReference
     private lateinit var mediaPlayer: MediaPlayer // For playing word pronunciation.
+    private lateinit var firebaseAuth: FirebaseAuth
 
     companion object {
         fun newInstance(): WordOfTheDayFragment {
@@ -43,8 +46,6 @@ class WordOfTheDayFragment: Fragment() {
         val policy: StrictMode.ThreadPolicy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
 
-        database = FirebaseDatabase.getInstance()
-
         val wotdDateTV = view.findViewById<TextView>(R.id.wotdDateTV)
         val wotdTV = view.findViewById<TextView>(R.id.wotdTV)
         val wotdPronounceFAB = view.findViewById<FloatingActionButton>(R.id.wotdPronounceFAB)
@@ -55,10 +56,19 @@ class WordOfTheDayFragment: Fragment() {
         val wotdENExampleTV = view.findViewById<TextView>(R.id.wotdENExampleTV)
         val wotdLoveFAB = view.findViewById<FloatingActionButton>(R.id.wotdLoveFAB)
         val wotdShareFAB = view.findViewById<FloatingActionButton>(R.id.wotdShareFAB)
-
+        val progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
         val currentDate = LocalDate.now().format(DateTimeFormatter.ofPattern("d-M-yyyy"))
 
-        database.reference.child("wordOfTheDay").child(currentDate).addValueEventListener(object : ValueEventListener {
+        // Show progress bar until Firebase retrieves data from the database.
+        progressBar.visibility = View.VISIBLE
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        val currentUser = firebaseAuth.currentUser
+
+        // Query Firebase Database for current word of the day.
+        databaseRef = FirebaseDatabase.getInstance().reference
+        databaseRef.child("wordOfTheDay").child(currentDate)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
                 Timber.tag("wordOfTheDay").d(p0.toException())
             }
@@ -73,19 +83,88 @@ class WordOfTheDayFragment: Fragment() {
                 wotdTransliterationTV.text = currentWOTD?.wordTransliteration
                 wotdTypeTV.text = currentWOTD?.wordType
                 wotdDefinitionTV.text = currentWOTD?.wordDefinition
+                wotdPronounceFAB.visibility = View.VISIBLE
                 if (currentWOTD?.exampleSentenceEN != "") {
                     wotdENExampleTV.text = currentWOTD?.exampleSentenceEN
                     wotdBGExampleTV.text = currentWOTD?.exampleSentenceBG
                 }
 
                 // Create MediaPlayer instance that uses URL from Google Cloud Storage.
-                // to play word pronunciation
+                // to play word pronunciation.
                 mediaPlayer = MediaPlayer.create(context, Uri.parse(currentWOTD?.pronunciationURL))
-                mediaPlayer.setAudioAttributes(AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).build()
+                mediaPlayer
+                    .setAudioAttributes(
+                        AudioAttributes.Builder()
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .build()
                 )
 
-                // Play word pronunciation on click of FAB.
+                // Play word pronunciation on click of pronounce FAB.
                 wotdPronounceFAB.setOnClickListener { mediaPlayer.start() }
+
+                // Reference to current user's collection of favourite words.
+                val favWordRef = databaseRef.child("users").child(currentUser!!.uid)
+                    .child("favWords").child(currentDate)
+
+                // Query user's favourite words collection
+                // to check if today's word of the day exists there.
+                favWordRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onCancelled(p0: DatabaseError) {
+                        Timber.tag("favWord").d(p0.toException())
+                    }
+
+                    override fun onDataChange(p0: DataSnapshot) {
+                        // Variable used to determine what action wotdLoveFAB will do.
+                        var loveFABAction: String?
+
+                        // Hide progress bar.
+                        progressBar.visibility = View.GONE
+
+                        // Show widgets.
+                        wotdLoveFAB.visibility = View.VISIBLE
+                        wotdShareFAB.visibility = View.VISIBLE
+
+                        if (p0.exists()) {
+                            // Word exists in user's collection.
+                            // Set action to remove the word from the db if the button is clicked.
+                            // Set colour to red.
+                            loveFABAction = "hate"
+                            wotdLoveFAB.backgroundTintList = ColorStateList.valueOf(Color.RED)
+                        } else {
+                            // Word doesn't exists in user's collection.
+                            // Set action to add the word to the db if the button is clicked.
+                            // Set colour to default.
+                            loveFABAction = "love"
+                            wotdLoveFAB.backgroundTintList =
+                                ColorStateList.valueOf(
+                                    ResourcesCompat
+                                        .getColor(resources, R.color.colorPrimary, null)
+                                )
+                        }
+
+                        // Determine what action to take on click of the button (add/remove from db).
+                        wotdLoveFAB.setOnClickListener {
+                            if (loveFABAction == "love") {
+                                loveFABAction = "hate"
+                                // Add the word to the db.
+                                // Set button colour to red.
+                                favWordRef.setValue(currentWOTD)
+                                wotdLoveFAB.backgroundTintList = ColorStateList.valueOf(Color.RED)
+                            } else {
+                                loveFABAction = "love"
+                                // Remove the word from the db.
+                                // Reset button colour.
+                                favWordRef.removeValue()
+                                wotdLoveFAB.backgroundTintList =
+                                    ColorStateList.valueOf(
+                                        ResourcesCompat
+                                            .getColor(resources, R.color.colorPrimary, null)
+                                    )
+                            }
+                        }
+                    }
+
+                })
             }
         })
 
