23fb278a07ddb0154b05952460c76af7580af79c
Tsvetoslav Dimov <baskski@gmail.com>
2020-01-18 23:19:07 +0000

    Added functionality to pick random 'Word of the Day' from the list of previously selected words, when the user requests it.

--- app/src/main/java/bask/lingvino/fragments/WordOfTheDayFragment.kt
+++ app/src/main/java/bask/lingvino/fragments/WordOfTheDayFragment.kt
@@ -26,6 +26,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.skydoves.balloon.ArrowOrientation
+import com.skydoves.balloon.Balloon
 import com.skydoves.balloon.BalloonAnimation
 import com.skydoves.balloon.createBalloon
 import timber.log.Timber
@@ -40,6 +41,19 @@ class WordOfTheDayFragment: Fragment() {
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var sharedPref: SharedPreferences
     private lateinit var date: String
+
+    private lateinit var wotdDateTV: TextView
+    private lateinit var wotdTV: TextView
+    private lateinit var wotdPronounceFAB: FloatingActionButton
+    private lateinit var wotdRandomFAB: FloatingActionButton
+    private lateinit var wotdTransliterationTV: TextView
+    private lateinit var wotdTypeTV: TextView
+    private lateinit var wotdDefinitionTV: TextView
+    private lateinit var wotdExampleTV: TextView
+    private lateinit var wotdLoveFAB: FloatingActionButton
+    private lateinit var wotdListFAB: FloatingActionButton
+    private lateinit var progressBar: ProgressBar
+    private lateinit var balloon: Balloon
 
     companion object {
         private const val argKey = "date"
@@ -67,19 +81,20 @@ class WordOfTheDayFragment: Fragment() {
         // Change toolbar title.
         (activity as? AppCompatActivity)?.supportActionBar?.title = resources.getString(R.string.wotd)
 
-        val wotdDateTV = view.findViewById<TextView>(R.id.wotdDateTV)
-        val wotdTV = view.findViewById<TextView>(R.id.wotdTV)
-        val wotdPronounceFAB = view.findViewById<FloatingActionButton>(R.id.wotdPronounceFAB)
-        val wotdTransliterationTV = view.findViewById<TextView>(R.id.wotdTransliterationTV)
-        val wotdTypeTV = view.findViewById<TextView>(R.id.wotdTypeTV)
-        val wotdDefinitionTV = view.findViewById<TextView>(R.id.wotdDefinitionTV)
-        val wotdExampleTV = view.findViewById<TextView>(R.id.wotdExampleTV)
-        val wotdLoveFAB = view.findViewById<FloatingActionButton>(R.id.wotdLoveFAB)
-        val wotdListFAB = view.findViewById<FloatingActionButton>(R.id.wotdListFAB)
-        val progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
+        wotdDateTV = view.findViewById(R.id.wotdDateTV)
+        wotdTV = view.findViewById(R.id.wotdTV)
+        wotdPronounceFAB = view.findViewById(R.id.wotdPronounceFAB)
+        wotdRandomFAB = view.findViewById(R.id.wotdRandomFAB)
+        wotdTransliterationTV = view.findViewById(R.id.wotdTransliterationTV)
+        wotdTypeTV = view.findViewById(R.id.wotdTypeTV)
+        wotdDefinitionTV = view.findViewById(R.id.wotdDefinitionTV)
+        wotdExampleTV = view.findViewById(R.id.wotdExampleTV)
+        wotdLoveFAB = view.findViewById(R.id.wotdLoveFAB)
+        wotdListFAB = view.findViewById(R.id.wotdListFAB)
+        progressBar = view.findViewById(R.id.progressBar)
 
         // Create balloon tooltip for later use.
-        val balloon = createBalloon(context!!) {
+        balloon = createBalloon(context!!) {
             setArrowOrientation(ArrowOrientation.LEFT)
             setWidthRatio(0.6f)
             setHeight(70)
@@ -97,6 +112,48 @@ class WordOfTheDayFragment: Fragment() {
             setShowTime(1)
         }
 
+        // Show progress bar until Firebase retrieves data from the database.
+        progressBar.visibility = View.VISIBLE
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        databaseRef = FirebaseDatabase.getInstance().reference
+
+        // Query Firebase Database for word of the day.
+        getWord(date)
+
+        wotdRandomFAB.setOnClickListener {
+            // Show progress bar until Firebase retrieves data from the database.
+            progressBar.visibility = View.VISIBLE
+            databaseRef.child("wordOfTheDay").child("past")
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onCancelled(p0: DatabaseError) {
+                        // Hide progress bar.
+                        progressBar.visibility = View.GONE
+                        Timber.tag("random").e(p0.toException())
+                    }
+
+                    override fun onDataChange(p0: DataSnapshot) {
+                        // Select a random word from the list of previously selected WOTD.
+                        val dates = mutableListOf<String>()
+                        p0.children.forEach { dates.add(it.key!!) }
+                        fragmentManager!!
+                            .beginTransaction()
+                            .replace(
+                                R.id.fragmentContainer,
+                                newInstance(dates.random())
+                            )
+                            .addToBackStack(null)
+                            .commit()
+
+                        // Hide progress bar.
+                        progressBar.visibility = View.GONE
+                    }
+
+                })
+        }
+    }
+
+    private fun getWord(date: String) {
         sharedPref = activity!!.getSharedPreferences("learnBulgarian", 0)
 
         // Check if the user had previously visited this fragment.
@@ -110,150 +167,147 @@ class WordOfTheDayFragment: Fragment() {
                 false
             } else true
 
-        // Show progress bar until Firebase retrieves data from the database.
-        progressBar.visibility = View.VISIBLE
-
-        firebaseAuth = FirebaseAuth.getInstance()
         val currentUser = firebaseAuth.currentUser
 
-        // Query Firebase Database for current word of the day.
-        databaseRef = FirebaseDatabase.getInstance().reference
-        databaseRef.child("wordOfTheDay").child(date)
+        databaseRef.child("wordOfTheDay").child("past").child(date)
             .addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {
-                Timber.tag("wordOfTheDay").d(p0.toException())
-            }
-
-            override fun onDataChange(p0: DataSnapshot) {
-                // Current word of the day.
-                val currentWOTD = p0.getValue(WordOfTheDay::class.java)!!
-
-                // Set values for all widgets.
-                wotdDateTV.text = LocalDate.now().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM))
-                wotdTV.text = currentWOTD.word
-                wotdTransliterationTV.text = currentWOTD.wordTransliteration
-                wotdTypeTV.text = currentWOTD.wordType
-                wotdDefinitionTV.text = currentWOTD.wordDefinition
-                wotdPronounceFAB.visibility = View.VISIBLE
-                highlightWord(currentWOTD.exampleSentenceBG, currentWOTD.word, wotdExampleTV)
-
-                // Show balloon tooltip to attract attention to the switchable
-                // English/Bulgarian translation.
-                if (!returningUser) balloon.showAlignRight(wotdExampleTV)
-
-                // Create MediaPlayer instance that uses URL from Google Cloud Storage.
-                // to play word pronunciation.
-                mediaPlayer = MediaPlayer.create(context, Uri.parse(currentWOTD.pronunciationURL))
-                    .apply {
-                        setAudioAttributes(
-                            AudioAttributes.Builder()
-                                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                                .build()
-                        )
-                    }
-
-
-                // Play word pronunciation on click of pronounce FAB.
-                wotdPronounceFAB.setOnClickListener { mediaPlayer.start() }
-
-                // Switch between English and Bulgarian example sentences.
-                wotdExampleTV.setOnClickListener {
-                    if (wotdExampleTV.text.toString() == currentWOTD.exampleSentenceBG) {
-                        highlightWord(
-                            currentWOTD.exampleSentenceEN,
-                            currentWOTD.wordEng,
-                            wotdExampleTV
-                        )
-                    } else {
-                        highlightWord(
-                            currentWOTD.exampleSentenceBG,
-                            currentWOTD.word,
-                            wotdExampleTV
-                        )
-                    }
+                override fun onCancelled(p0: DatabaseError) {
+                    Timber.tag("wordOfTheDay").d(p0.toException())
                 }
 
-                // Reference to current user's collection of favourite words.
-                val favWordRef = databaseRef.child("users")
-                    .child(currentUser!!.uid)
-                    .child("favWords")
+                override fun onDataChange(p0: DataSnapshot) {
+                    // Selected word of the day.
+                    val currentWOTD = p0.getValue(WordOfTheDay::class.java)!!
 
-                // Query user's favourite words collection
-                // to check if today's word of the day exists there.
-                favWordRef.addValueEventListener(object : ValueEventListener {
-                    override fun onCancelled(p0: DatabaseError) {
-                        Timber.tag("favWord").d(p0.toException())
+                    // Set values for all widgets.
+                    wotdDateTV.text = LocalDate.now()
+                        .format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM))
+                    wotdTV.text = currentWOTD.word
+                    wotdTransliterationTV.text = currentWOTD.wordTransliteration
+                    wotdTypeTV.text = currentWOTD.wordType
+                    wotdDefinitionTV.text = currentWOTD.wordDefinition
+                    wotdPronounceFAB.visibility = View.VISIBLE
+                    wotdRandomFAB.visibility = View.VISIBLE
+                    highlightWord(currentWOTD.exampleSentenceBG, currentWOTD.word, wotdExampleTV)
+
+                    // Show balloon tooltip to attract attention to the switchable
+                    // English/Bulgarian translation.
+                    if (!returningUser) balloon.showAlignRight(wotdExampleTV)
+
+                    // Create MediaPlayer instance that uses URL from Google Cloud Storage.
+                    // to play word pronunciation.
+                    mediaPlayer =
+                        MediaPlayer.create(context, Uri.parse(currentWOTD.pronunciationURL))
+                            .apply {
+                                setAudioAttributes(
+                                    AudioAttributes.Builder()
+                                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                                        .build()
+                                )
+                            }
+
+
+                    // Play word pronunciation on click of pronounce FAB.
+                    wotdPronounceFAB.setOnClickListener { mediaPlayer.start() }
+
+                    // Switch between English and Bulgarian example sentences.
+                    wotdExampleTV.setOnClickListener {
+                        if (wotdExampleTV.text.toString() == currentWOTD.exampleSentenceBG) {
+                            highlightWord(
+                                currentWOTD.exampleSentenceEN,
+                                currentWOTD.wordEng,
+                                wotdExampleTV
+                            )
+                        } else {
+                            highlightWord(
+                                currentWOTD.exampleSentenceBG,
+                                currentWOTD.word,
+                                wotdExampleTV
+                            )
+                        }
                     }
 
-                    override fun onDataChange(p0: DataSnapshot) {
-                        // Variable used to determine what action wotdLoveFAB will do.
-                        var loveFABAction: String?
+                    // Reference to current user's collection of favourite words.
+                    val favWordRef = databaseRef.child("users")
+                        .child(currentUser!!.uid)
+                        .child("favWords")
 
-                        // Hide progress bar.
-                        progressBar.visibility = View.GONE
-
-                        // Show buttons.
-                        wotdLoveFAB.visibility = View.VISIBLE
-                        wotdListFAB.visibility = View.VISIBLE
-
-                        if (p0.hasChild(date)) {
-                            // Word exists in user's collection.
-                            // Set action to remove the word from the db if the button is clicked.
-                            // Set colour to red.
-                            loveFABAction = "hate"
-                            wotdLoveFAB.backgroundTintList = ColorStateList.valueOf(Color.RED)
-                        } else {
-                            // Word doesn't exists in user's collection.
-                            // Set action to add the word to the db if the button is clicked.
-                            // Set colour to default.
-                            loveFABAction = "love"
-                            if (isAdded) wotdLoveFAB.backgroundTintList =
-                                ColorStateList.valueOf(
-                                    ResourcesCompat
-                                        .getColor(resources, R.color.colorPrimary, null)
-                                )
+                    // Query user's favourite words collection
+                    // to check if today's word of the day exists there.
+                    favWordRef.addValueEventListener(object : ValueEventListener {
+                        override fun onCancelled(p0: DatabaseError) {
+                            Timber.tag("favWord").d(p0.toException())
                         }
 
-                        // Determine what action to take on click of the button (add/remove from db).
-                        wotdLoveFAB.setOnClickListener {
-                            if (loveFABAction == "love") {
+                        override fun onDataChange(p0: DataSnapshot) {
+                            // Variable used to determine what action wotdLoveFAB will do.
+                            var loveFABAction: String?
+
+                            // Hide progress bar.
+                            progressBar.visibility = View.GONE
+
+                            // Show buttons.
+                            wotdLoveFAB.visibility = View.VISIBLE
+                            wotdListFAB.visibility = View.VISIBLE
+
+                            if (p0.hasChild(date)) {
+                                // Word exists in user's collection.
+                                // Set action to remove the word from the db if the button is clicked.
+                                // Set colour to red.
                                 loveFABAction = "hate"
-                                // Add the word to the db.
-                                // Set button colour to red.
-                                favWordRef.child(date).setValue(currentWOTD)
                                 wotdLoveFAB.backgroundTintList = ColorStateList.valueOf(Color.RED)
                             } else {
+                                // Word doesn't exists in user's collection.
+                                // Set action to add the word to the db if the button is clicked.
+                                // Set colour to default.
                                 loveFABAction = "love"
-                                // Remove the word from the db.
-                                // Reset button colour.
-                                favWordRef.child(date).removeValue()
-                                wotdLoveFAB.backgroundTintList =
+                                if (isAdded) wotdLoveFAB.backgroundTintList =
                                     ColorStateList.valueOf(
                                         ResourcesCompat
                                             .getColor(resources, R.color.colorPrimary, null)
                                     )
                             }
+
+                            // Determine what action to take on click of the button (add/remove from db).
+                            wotdLoveFAB.setOnClickListener {
+                                if (loveFABAction == "love") {
+                                    loveFABAction = "hate"
+                                    // Add the word to the db.
+                                    // Set button colour to red.
+                                    favWordRef.child(date).setValue(currentWOTD)
+                                    wotdLoveFAB.backgroundTintList =
+                                        ColorStateList.valueOf(Color.RED)
+                                } else {
+                                    loveFABAction = "love"
+                                    // Remove the word from the db.
+                                    // Reset button colour.
+                                    favWordRef.child(date).removeValue()
+                                    wotdLoveFAB.backgroundTintList =
+                                        ColorStateList.valueOf(
+                                            ResourcesCompat
+                                                .getColor(resources, R.color.colorPrimary, null)
+                                        )
+                                }
+                            }
+
+                            // Open user's favourite words collection in a new Fragment.
+                            wotdListFAB.setOnClickListener {
+                                fragmentManager!!
+                                    .beginTransaction()
+                                    .replace(
+                                        R.id.fragmentContainer,
+                                        WordOfTheDayFavouritesFragment.newInstance()
+                                    )
+                                    .addToBackStack(null)
+                                    .commit()
+                            }
+
+                            wotdListFAB.isEnabled = p0.exists()
                         }
 
-                        // Open user's favourite words collection in a new Fragment.
-                        wotdListFAB.setOnClickListener {
-                            fragmentManager!!
-                                .beginTransaction()
-                                .replace(
-                                    R.id.fragmentContainer,
-                                    WordOfTheDayFavouritesFragment.newInstance()
-                                )
-                                .addToBackStack(null)
-                                .commit()
-                        }
-
-                        wotdListFAB.isEnabled = p0.exists()
-                    }
-
-                })
-            }
-        })
-
+                    })
+                }
+            })
     }
 
     // Highlights every occurrence of a word in text.
