2dc940034ceb2cfa91ecdcd951885c422cf0dbd4
Tsvetoslav Dimov <baskski@gmail.com>
2020-03-28 13:42:32 +0000

    Implement filtering user scores by month and year in either ascending or descending order.

--- app/src/main/java/bask/lingvino/fragments/QuizLeaderboard.kt
+++ app/src/main/java/bask/lingvino/fragments/QuizLeaderboard.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -18,12 +19,17 @@ import com.google.firebase.database.*
 import com.kizitonwose.calendarview.utils.yearMonth
 import org.threeten.bp.LocalDate
 import timber.log.Timber
+import kotlin.collections.ArrayList
 
-class QuizLeaderboard : Fragment() {
+class QuizLeaderboard : Fragment(), View.OnClickListener {
 
     private lateinit var usersRef: DatabaseReference
     private lateinit var fbUser: FirebaseUser
     private lateinit var leaderboardRV: RecyclerView
+    private lateinit var leaderboardAdapter: LeaderboardAdapter
+    private lateinit var monthHeader: TextView
+    private lateinit var yearHeader: TextView
+    private lateinit var leaderboardUsers: MutableList<LeaderboardUser>
     private val date = LocalDate.now()
 
     companion object {
@@ -41,6 +47,8 @@ class QuizLeaderboard : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        monthHeader = view.findViewById(R.id.monthHeader)
+        yearHeader = view.findViewById(R.id.yearHeader)
         leaderboardRV = view.findViewById<RecyclerView>(R.id.leaderboardRV).apply {
             val lm = LinearLayoutManager(context)
             layoutManager = lm
@@ -52,16 +60,19 @@ class QuizLeaderboard : Fragment() {
         fbUser = FirebaseAuth.getInstance().currentUser!!
 
         loadUsersData()
+
+        monthHeader.setOnClickListener(this)
+        yearHeader.setOnClickListener(this)
     }
 
-    private fun loadUsersData(sortBy: String = "month", order: String = "descending") {
+    private fun loadUsersData() {
         usersRef.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
                 Timber.tag("loadUsersData()").e(p0.toException())
             }
 
             override fun onDataChange(p0: DataSnapshot) {
-                val leaderboardUsers = mutableListOf<LeaderboardUser>()
+                leaderboardUsers = mutableListOf() // Initialise the list.
                 p0.children.forEach {
                     LeaderboardUser().apply {
                         userID = it.key!!
@@ -89,23 +100,13 @@ class QuizLeaderboard : Fragment() {
                     }
                 }
 
-                when (sortBy) {
-                    "month" ->
-                        leaderboardUsers.sortWith( // First sort by monthly score, then by yearly score.
-                            compareBy({ it.currentMonthScore }, { it.currentYearScore })
-                        )
-                    "year" -> {
-                        leaderboardUsers.sortWith( // First sort by yearly score, then by monthly score.
-                            compareBy({ it.currentYearScore }, { it.currentMonthScore })
-                        )
-                    }
-                }
-
-                if (order == "descending") leaderboardUsers.reverse()
-
-                leaderboardRV.adapter = LeaderboardAdapter(
+                leaderboardAdapter = LeaderboardAdapter(
                     leaderboardUsers as ArrayList<LeaderboardUser>
                 )
+                leaderboardRV.adapter = leaderboardAdapter
+
+                // Filter by month in descending order by default.
+                performFiltering("month", "descending")
             }
         })
     }
@@ -115,5 +116,53 @@ class QuizLeaderboard : Fragment() {
         // Change toolbar title.
         (activity as? AppCompatActivity)?.supportActionBar?.title =
             resources.getString(R.string.quizLeaderboardTitle)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.monthHeader -> { // Filter by current month's score.
+                if (monthHeader.text == resources.getString(R.string.leaderboard_month_desc)) {
+                    monthHeader.text = resources.getString(R.string.leaderboard_month_asc)
+                    performFiltering("month", "ascending")
+                }
+                else {
+                    monthHeader.text = resources.getString(R.string.leaderboard_month_desc)
+                    performFiltering("month", "descending")
+                }
+                // Update year header.
+                yearHeader.text = resources.getString(R.string.leaderboard_year)
+            }
+            R.id.yearHeader -> { // Filter by yearly score.
+                if (yearHeader.text == resources.getString(R.string.leaderboard_year_desc)) {
+                    yearHeader.text = resources.getString(R.string.leaderboard_year_asc)
+                    performFiltering("year", "ascending")
+                }
+                else {
+                    yearHeader.text = resources.getString(R.string.leaderboard_year_desc)
+                    performFiltering("year", "descending")
+                }
+                // Update month header.
+                monthHeader.text = resources.getString(R.string.leaderboard_month)
+            }
+        }
+    }
+
+    private fun performFiltering(filter: String, order: String) {
+        when (filter) {
+            "month" ->
+                leaderboardUsers.sortWith( // First sort by monthly score, then by yearly score.
+                    compareBy({ it.currentMonthScore }, { it.currentYearScore })
+                )
+            "year" -> {
+                leaderboardUsers.sortWith( // First sort by yearly score, then by monthly score.
+                    compareBy({ it.currentYearScore }, { it.currentMonthScore })
+                )
+            }
+        }
+
+        if (order == "descending") leaderboardUsers.reverse()
+
+        // Display the results.
+        leaderboardRV.adapter = LeaderboardAdapter(leaderboardUsers as ArrayList<LeaderboardUser>)
     }
 }
