11a00e53aec2da468d1507b61055f8dfc89b93f3
Tsvetoslav Dimov <baskski@gmail.com>
2020-03-29 17:47:44 +0100

    All languages supported in Dashboard fragment.

--- app/src/main/java/bask/lingvino/fragments/DashboardFragment.kt
+++ app/src/main/java/bask/lingvino/fragments/DashboardFragment.kt
@@ -13,6 +13,7 @@ import bask.lingvino.R
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import timber.log.Timber
 
 class DashboardFragment : Fragment(), View.OnClickListener {
 
@@ -22,6 +23,10 @@ class DashboardFragment : Fragment(), View.OnClickListener {
     private lateinit var languageOptionsCard: CardView
     private lateinit var progressBar: ProgressBar
     private lateinit var welcomeTV: TextView
+    private lateinit var wotdInfo: TextView
+    private lateinit var translatorInfo: TextView
+    private lateinit var quizInfo: TextView
+    private lateinit var languageOptionsInfo: TextView
     private lateinit var navView: NavigationView
 
     companion object {
@@ -47,8 +52,18 @@ class DashboardFragment : Fragment(), View.OnClickListener {
         navView = activity!!.findViewById(R.id.navView)
         progressBar = view.findViewById(R.id.progressBar)
         welcomeTV = view.findViewById(R.id.welcomeTV)
+        wotdInfo = view.findViewById(R.id.wotdInfo)
+        translatorInfo = view.findViewById(R.id.translatorInfo)
+        quizInfo = view.findViewById(R.id.quizInfo)
+        languageOptionsInfo = view.findViewById(R.id.languageOptionsInfo)
 
-        displayWelcomeMessage(FirebaseAuth.getInstance().currentUser!!.uid)
+        val sharedPref = activity?.getSharedPreferences("learnBulgarian", 0)
+        val spokenLanguage = sharedPref?.getString("SPOKEN_LANG_NAME", "Bulgarian")
+
+        Timber.tag("seleccc").d(spokenLanguage)
+
+        displayWelcomeMessage(FirebaseAuth.getInstance().currentUser!!.uid, spokenLanguage)
+        displayCardsInfo(spokenLanguage)
 
         // Set click listeners to the CardView items.
         wotdCard.setOnClickListener(this)
@@ -57,7 +72,36 @@ class DashboardFragment : Fragment(), View.OnClickListener {
         languageOptionsCard.setOnClickListener(this)
     }
 
-    private fun displayWelcomeMessage(userID: String) {
+    private fun displayCardsInfo(spokenLanguage: String?) {
+        when(spokenLanguage) {
+            "Bulgarian" -> {
+                wotdInfo.text = resources.getString(R.string.wotd_info_bg)
+                translatorInfo.text = resources.getString(R.string.translator_info_bg)
+                quizInfo.text = resources.getString(R.string.quiz_info_bg)
+                languageOptionsInfo.text = resources.getString(R.string.lang_options_info_bg)
+            }
+            "Spanish" -> {
+                wotdInfo.text = resources.getString(R.string.wotd_info_es)
+                translatorInfo.text = resources.getString(R.string.translator_info_es)
+                quizInfo.text = resources.getString(R.string.quiz_info_es)
+                languageOptionsInfo.text = resources.getString(R.string.lang_options_info_es)
+            }
+            "Russian" -> {
+                wotdInfo.text = resources.getString(R.string.wotd_info_ru)
+                translatorInfo.text = resources.getString(R.string.translator_info_ru)
+                quizInfo.text = resources.getString(R.string.quiz_info_ru)
+                languageOptionsInfo.text = resources.getString(R.string.lang_options_info_ru)
+            }
+            else -> {
+                wotdInfo.text = resources.getString(R.string.wotd_info_en)
+                translatorInfo.text = resources.getString(R.string.translator_info_en)
+                quizInfo.text = resources.getString(R.string.quiz_info_en)
+                languageOptionsInfo.text = resources.getString(R.string.lang_options_info_en)
+            }
+        }
+    }
+
+    private fun displayWelcomeMessage(userID: String, spokenLanguage: String?) {
         progressBar.visibility = View.VISIBLE
         FirebaseDatabase.getInstance().reference
             .child("users/$userID").addListenerForSingleValueEvent(object : ValueEventListener {
@@ -67,7 +111,12 @@ class DashboardFragment : Fragment(), View.OnClickListener {
 
                 override fun onDataChange(p0: DataSnapshot) {
                     val username = p0.child("username").value as String
-                    welcomeTV.text = resources.getString(R.string.welcome_message, username)
+                    welcomeTV.text = when(spokenLanguage) {
+                        "Bulgarian" -> resources.getString(R.string.welcome_message_bg, username)
+                        "Spanish" -> resources.getString(R.string.welcome_message_es, username)
+                        "Russian" -> resources.getString(R.string.welcome_message_ru, username)
+                        else -> resources.getString(R.string.welcome_message_en, username)
+                    }
                     progressBar.visibility = View.GONE
                 }
             })
